c2eb019654ac0939a1919e34378752b2ee935240
Esteban <estebanoller73@gmail.com>
2024-02-27 19:40:35 -0300

    primer comit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <section className='section-port'>
+      <h1>Time Cristal</h1>
+      <p>Tu Bloc de nota re fachero xD</p>
+    </section>
+  )
+}
+
+export default App
